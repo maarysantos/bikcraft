c1f25eac93342bc8421b79f9624e4aabf03d8106
MaarySantos <maary.santos1@gmail.com>
2019-12-04 14:23:09 -0300

    Add navegação com javascript

---
+++ js/script.js
@@ -0,0 +1,14 @@
+if (window.SimpleSlide) {
+  new SimpleSlide({
+  slide: "quote", // nome do atributo data-slide="principal"
+  time: 5000, // tempo de transição dos slides
+});
+
+new SimpleSlide({
+  slide: "portfolio", // nome do atributo data-slide="principal"
+  nav: true, // se deve ou não mostrar a navegação
+  auto: true, // se o slide deve passar automaticamente
+  time: 5000, // tempo de transição dos slides
+  pauseOnHover: true // pausa a transição automática
+});
+}
